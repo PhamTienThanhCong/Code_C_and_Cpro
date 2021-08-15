fb9f11e62a0d72c6822861aa32ba9ed04dd8f244
cong <cong.pttc@gmail.com>
2021-08-15 16:03:26 +0700

    add pass one

--- basic_problem_1-25/Bai_15.cpp
+++ basic_problem_1-25/Bai_15.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main(){
     int m,n;
-    cin>>m>>n;
-    int a[n][m],v[n][m];
+    cin>>n>>m;
+    int a[n][m],v[n][m],sl=0;
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=m;j++){
+            cin>>a[i][j];
+        }
+    }
 }
