12d0944de8d8c2257129cb328973226695767e88
cong <cong.pttc@gmail.com>
2021-08-16 10:16:07 +0700

    luu tam

--- basic_problem_1-25/Bai_15.cpp
+++ basic_problem_1-25/Bai_15.cpp
@@ -5,10 +5,8 @@ using namespace std;
 int main(){
     int m,n;
     cin>>n>>m;
-    int a[n][m],v[n][m],sl=0;
-    for (int i=1;i<=n;i++){
-        for (int j=1;j<=m;j++){
-            cin>>a[i][j];
-        }
+    int a[n*m+1],sl=0;
+    for (int i=1;i<=n*m;i++){
+        cin>>a[i];
     }
-}
+    
