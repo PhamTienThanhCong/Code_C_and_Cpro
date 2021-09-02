6f7f092d478de252a15d932b3128c2743b166004
cong <cong.pttc@gmail.com>
2021-09-02 17:32:32 +0700

    olp chuyen

---
+++ C++/code_fores/olp_chuyen_2020/REFLECTIVE.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long int lf,rt;
+
+long int Dem(long int a){
+    long int Tong=0,i=2,t=1,temp;
+    while (a/i>0){
+        temp=(a/i)-a/(i*2);
+        Tong+=temp*t;
+        i=i*2;
+        t++;
+    }
+    return Tong;
+}
+
+int main(){
+    cin>>lf>>rt;
+    cout<<Dem(rt)-Dem(lf);
+}
