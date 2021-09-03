1aead7782dfff9852b9389a376c4a8188aba3aa3
cong <cong.pttc@gmail.com>
2021-09-03 15:03:30 +0700

    bài 15 vẫn lỗi ???

--- C++/code_fores/basic_problem_1-25/Bai_15.cpp
+++ C++/code_fores/basic_problem_1-25/Bai_15.cpp
@@ -2,12 +2,72 @@
 
 using namespace std;
 
-int main(){
-    int m,n;
-    cin>>n>>m;
-    int a[n*m+1],sl=0,h=1;
-    for (int i=1;i<=n*m;i++){
-        cin>>a[i];
+int a[100][100],sl=0,h=1;
+int m,n;
+
+bool tinh(int sky,int a[][100],int i, int j,int n, int m){
+    int temp=0;
+    for (int l=i+1;i<=n;l++){
+        if (a[l][j]>sky){
+            temp=1;
+            break;
+        }
     }
+    if (temp==0){
+        return true;
+    }
+
+    temp=0;
+    for (int l=i-1;l>=1;l--){
+        if (a[l][j]>sky){
+            temp=1;
+            break;
+        }
+    }
+    if (temp==0){
+        return true;
+    }
+
+    temp=0;
+    for (int l=j+1;l<=m;l++){
+        if (a[i][l]>sky){
+            temp=1;
+            break;
+        }
+    }
+    if (temp==0){
+        return true;
+    }
+
+    temp=0;
+    for (int l=j-1;l>=1;l--){
+        if (a[i][l]>sky){
+            temp=1;
+            break;
+        }
+    }
+    if (temp==0){
+        return true;
+    }
+    return false;
+
+}
+
+int main(){
+    cin>>n>>m;
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin>>a[i][j];
+        }
+    }
+    sl=n*2+m*2-4;
+    for (int i=2;i<n;i++){
+        for(int j=2;j<m;j++){
+            if(tinh(a[i][j],a,i,j,n,m)){
+                sl++;
+            }
+        }
+    }
+    cout<<sl;
 }
     
