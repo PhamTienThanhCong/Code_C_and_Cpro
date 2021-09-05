e804ca84620a62742a50424b6a6055e249d45856
cong <cong.pttc@gmail.com>
2021-09-05 14:52:11 +0700

    bài 17 ez mà đề thì ối dồi ôi

--- C++/code_fores/basic_problem_1-25/Bai_17.cpp
+++ C++/code_fores/basic_problem_1-25/Bai_17.cpp
@@ -2,14 +2,34 @@
 
 using namespace std;
 
+void change(int *a, int *b){
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+    
+}
+
 int main(){
-    int _min=1000,_max=-1000;
     int n,x[101],y[101];
     cin>>n;
     for (int i=1;i<=n;i++){
         cin>>x[i]>>y[i];
-        _max = max(_max,x[i]);
-        _min = min(_min,x[i]);
     }
-    cout<<_min<<_max;
+    for (int i=1;i<=n;i++){
+        for (int j=i;j<=n;j++){
+            if (y[i]>y[j]){
+                change(x+i,x+j);
+                change(y+i,y+j);
+            }
+        }
+    }
+    int Y_way;
+    Y_way=int(float(n)/2+0.5);
+    Y_way=y[Y_way];
+
+    int sum=0;
+    for (int i=1;i<=n;i++){
+        sum = sum + max(Y_way,y[i])-min(Y_way,y[i]);
+    }
+    cout<<sum;
 }
