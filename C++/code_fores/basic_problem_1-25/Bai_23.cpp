73201681319858bff70b0053f5769546479ba287
cong <cong.pttc@gmail.com>
2021-09-08 16:04:55 +0700

    Bai 23 part 1

---
+++ C++/code_fores/basic_problem_1-25/Bai_23.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,sl=0;
+    int a[101],b[101][3];
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    for (int i=1;i<n;i++){
+        for (int j=i+1;j<=n;j++){
+            if (a[i]==a[j]){
+                sl++;
+                b[sl][1]=i;
+                b[sl][2]=j;
+            }
+        }
+    }
+    cout<<sl<<'\n';
+    for (int i=1;i<=sl;i++){
+        cout<<b[i][1]<<' '<<b[i][2]<<'\n';
+    }
+}
