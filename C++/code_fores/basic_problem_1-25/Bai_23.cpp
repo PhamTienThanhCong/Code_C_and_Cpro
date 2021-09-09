c9e6c65f9181a2700722cfc8fd13a2787fc22fc4
cong <cong.pttc@gmail.com>
2021-09-09 10:06:55 +0700

    căn chỉnh

--- C++/code_fores/basic_problem_1-25/Bai_23.cpp
+++ C++/code_fores/basic_problem_1-25/Bai_23.cpp
@@ -1,25 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
-    int n,sl=0;
-    int a[101],b[1000000][3];
-    cin>>n;
-    for (int i=1;i<=n;i++){
-        cin>>a[i];
+int main()
+{
+    int n, sl = 0;
+    int a[101], b[1000000][3];
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
     }
-    for (int i=1;i<n;i++){
-        for (int j=i+1;j<=n;j++){
-            if (a[i]==a[j]){
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            if (a[i] == a[j])
+            {
                 sl++;
-                b[sl][1]=i;
-                b[sl][2]=j;
+                b[sl][1] = i;
+                b[sl][2] = j;
             }
         }
     }
-    cout<<sl<<'\n';
-    for (int i=1;i<=sl;i++){
-        cout<<b[i][1]<<' '<<b[i][2]<<'\n';
+    cout << sl << '\n';
+    for (int i = 1; i <= sl; i++)
+    {
+        cout << b[i][1] << ' ' << b[i][2] << '\n';
     }
 }
