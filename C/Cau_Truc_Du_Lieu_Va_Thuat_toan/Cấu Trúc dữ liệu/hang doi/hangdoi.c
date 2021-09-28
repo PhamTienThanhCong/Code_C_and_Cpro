4b9d860072f2b7f397e0c2872adf686adea65b40
congphamtienthanh <cong.pttc@gmail.com>
2021-09-28 11:20:39 +0700

    hàg ddoiwjd uplade

--- C/Cau_Truc_Du_Lieu_Va_Thuat_toan/Cấu Trúc dữ liệu/hang doi/hangdoi.c
+++ C/Cau_Truc_Du_Lieu_Va_Thuat_toan/Cấu Trúc dữ liệu/hang doi/hangdoi.c
@@ -6,7 +6,11 @@ int main(){
 	Queue *q = malloc(sizeof(Queue) ); 
  	q->ll.size = 0 ; 
  	q->ll.head= NULL ;
- 	printList(q->ll.head);
- 	
+	enqueue(q,10);
+	enqueue(q,20);
+	enqueue(q,30);
+ 	// printList(q->ll.head);
+	//printf("%d",dequeue(q)); //in xong xóa
+	//printf("%d\n",peek(q));  // in xong không xóa
+	
 } 
-
