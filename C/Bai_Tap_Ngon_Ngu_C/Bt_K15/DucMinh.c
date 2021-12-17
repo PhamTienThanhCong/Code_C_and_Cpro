978d8c9003e6e2af5aec9f97b912b72400083c1d
congphamtienthanh <cong.pttc@gmail.com>
2021-12-17 12:54:46 +0700

    add delete

--- C/Bai_Tap_Ngon_Ngu_C/Bt_K15/DucMinh.c
+++ C/Bai_Tap_Ngon_Ngu_C/Bt_K15/DucMinh.c
@@ -11,10 +11,16 @@ typedef struct QuanLySinhVien{
     float DiemTL;
 } QLSV;
 
-void QLSV_Nhap_Ds(int SoLuongSV, QLSV sinhvien[]){
+int SoLuongSV;
+QLSV sinhVien[100];
+
+void QLSV_Nhap_Ds(QLSV sinhvien[]){
     // system("cls");
     char c;
-    printf("\n\t1. NHAP THONG TIN SINH VIEN");
+    printf("\n\t1. NHAP THONG TIN SINH VIEN\n");
+    printf("Nhap So Luong Sinh Vien: ");
+    scanf("%d",&SoLuongSV);
+    scanf("%c",&c);
     for (int i=0; i< SoLuongSV; i++){
         printf("\n** Nhap thong tin sinh vien thu %d: **\n",i+1);
 
@@ -36,7 +42,7 @@ void QLSV_Nhap_Ds(int SoLuongSV, QLSV sinhvien[]){
     printf("----------Nhap Thong Tin Hoan Tat----------\n");
 }
 
-void QLSV_Tinh_Diem_Tich_Luy(int SoLuongSV, QLSV sinhvien[]){
+void QLSV_Tinh_Diem_Tich_Luy(QLSV sinhvien[]){
     // printf("\n\t2. TINH DIEM TICH LUY CUA SINH VIEN\n");
     for(int i=0; i< SoLuongSV; i++){
         sinhvien[i].DiemTL = (sinhvien[i].DiemTB + sinhvien[i].DiemRL)/2;
@@ -44,7 +50,7 @@ void QLSV_Tinh_Diem_Tich_Luy(int SoLuongSV, QLSV sinhvien[]){
     // printf("----------Tinh Diem Hoan Tat----------\n");
 }
 
-void QLSV_In_Thong_Tin(int SoLuongSV, QLSV sinhvien[]){
+void QLSV_In_Thong_Tin(QLSV sinhvien[]){
     printf("\n\t3. IN TOAN BO THONG TIN SINH VIEN:\n");
     printf("+STT+---Ma SV--+-------------Ho Ten-----------+--DiemTB--+--DiemRL--+--DiemTL--+\n");
     for (int i = 0; i < SoLuongSV; i++){
@@ -53,7 +59,7 @@ void QLSV_In_Thong_Tin(int SoLuongSV, QLSV sinhvien[]){
     }  
 }
 
-void QLSV_Xap_Xep_Diem_Ren_Luyen(int SoLuongSV, QLSV sinhvien[]){
+void QLSV_Xap_Xep_Diem_Ren_Luyen(QLSV sinhvien[]){
     // printf("\n\t4. XAP XEP SINH VIEN THEO DIEM REN LUYEN TANG DAN:\n");
     for (int i = 0; i < SoLuongSV; i++ ){
         for (int j = i+1; j < SoLuongSV; j++ ){
@@ -66,7 +72,7 @@ void QLSV_Xap_Xep_Diem_Ren_Luyen(int SoLuongSV, QLSV sinhvien[]){
     }
 }
 
-void QLSV_Sinh_Vien_Diem_Tich_Luy_Cao_Nhat(int SoLuongSV, QLSV sinhvien[]){
+void QLSV_Sinh_Vien_Diem_Tich_Luy_Cao_Nhat(QLSV sinhvien[]){
     int i = 0;
     for (int j = i+1; j < SoLuongSV; j++ ){
         if (sinhvien[i].DiemTL < sinhvien[j].DiemTL){
@@ -80,7 +86,7 @@ void QLSV_Sinh_Vien_Diem_Tich_Luy_Cao_Nhat(int SoLuongSV, QLSV sinhvien[]){
 
 }
 
-int QLSV_Them_Sv_Moi(int SoLuongSV, QLSV sinhvien[]){
+void QLSV_Them_Sv_Moi(QLSV sinhvien[]){
     printf("\n\t5. NHAP THONG TIN SINH VIEN MOI\n");
     int i = SoLuongSV;
     SoLuongSV++;
@@ -98,8 +104,7 @@ int QLSV_Them_Sv_Moi(int SoLuongSV, QLSV sinhvien[]){
     printf("- Nhap Diem Ren Luyen: ");
     scanf("%f", &sinhvien[i].DiemRL);
     
-    QLSV_Tinh_Diem_Tich_Luy(SoLuongSV, sinhvien);
-    return SoLuongSV;
+    QLSV_Tinh_Diem_Tich_Luy(sinhvien);
 }
 
 void remove_element(QLSV sinhvien[], int index, int array_length)
@@ -108,35 +113,36 @@ void remove_element(QLSV sinhvien[], int index, int array_length)
    for(i = index; i < array_length - 1; i++) sinhvien[i] = sinhvien[i + 1];
 }
 
-int QLSV_Xoa_Sinh_Vien(int SoLuongSV, QLSV sinhvien[]){
-    int slsv = SoLuongSV,j=0;
+void QLSV_Xoa_Sinh_Vien(QLSV sinhvien[]){
+    int slsv = SoLuongSV;
+    QLSV sv[1000];
     float hs;
     printf("\n\t 8. XOA SINH VIEN CO DIM TB THAP HON HS\n");
     printf("Nhap Diem HS: ");
     scanf("%f",&hs);
-    while (0<slsv){
-        if (sinhvien[j].DiemTB < hs){
-            remove_element(sinhvien, j, slsv);
-            slsv--;
-        }else{
+    int j = 0;
+    for (int i = 0 ; i < slsv; i++){
+        if (sinhvien[i].DiemTB>=hs){
+            sv[j] = sinhvien[i];
             j++;
         }
     }
-    return slsv;
+    SoLuongSV = j;
+    for (int i = 0 ; i < j ; i++){
+        sinhvien[i] = sv[i];
+    }
 }
 
 int main(){
-    QLSV sinhVien[100];
-    int n=2;
-    printf("%d",n);
-    QLSV_Nhap_Ds(n,sinhVien);
-    QLSV_Tinh_Diem_Tich_Luy(n,sinhVien);
-    QLSV_Xap_Xep_Diem_Ren_Luyen(n,sinhVien);
-    QLSV_In_Thong_Tin(n,sinhVien);
-    QLSV_Sinh_Vien_Diem_Tich_Luy_Cao_Nhat(n,sinhVien);
-    n = QLSV_Them_Sv_Moi(n,sinhVien);
-    QLSV_Xap_Xep_Diem_Ren_Luyen(n,sinhVien);
-    QLSV_In_Thong_Tin(n,sinhVien);
-    n = QLSV_Xoa_Sinh_Vien(n,sinhVien);
-    QLSV_In_Thong_Tin(n,sinhVien);
+    
+    QLSV_Nhap_Ds(sinhVien);
+    QLSV_Tinh_Diem_Tich_Luy(sinhVien);
+    QLSV_Xap_Xep_Diem_Ren_Luyen(sinhVien);
+    QLSV_In_Thong_Tin(sinhVien);
+    QLSV_Sinh_Vien_Diem_Tich_Luy_Cao_Nhat(sinhVien);
+    QLSV_Them_Sv_Moi(sinhVien);
+    QLSV_Xap_Xep_Diem_Ren_Luyen(sinhVien);
+    QLSV_In_Thong_Tin(sinhVien);
+    QLSV_Xoa_Sinh_Vien(sinhVien);
+    QLSV_In_Thong_Tin(sinhVien);
 }
