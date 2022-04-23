cbb377dc626c5e79894bae4b180b12c89b710234
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-23 14:20:08 +0700

    Cho chihuahua

--- C++/C nâng cao/oop_learning/Animal.cc
+++ C++/C nâng cao/oop_learning/Animal.cc
@@ -9,6 +9,7 @@ protected:
     string name, nameAnimail;
     float canNang, chieuCao;
 public:
+    Animail(){}
     Animail(int id, int soChan, string nameAnimail, string name, float canNang, float chieuCao){
         this->id = id;
         this->soChan = soChan;
@@ -25,23 +26,84 @@ public:
 };
 
 class dog : public Animail{
+    protected:
+    string mauLong;
     public: 
-    dog(int id, int soChan, string nameAnimail, string name, float canNang, float chieuCao){
+    dog(){}
+    dog(int id, int soChan, string nameAnimail, string name, float canNang, float chieuCao, string mauLong){
         this->id = id;
         this->soChan = soChan;
         this->nameAnimail = nameAnimail;
         this->name = name;
         this->canNang = canNang;
         this->chieuCao = chieuCao;
+        this->mauLong = mauLong;
+    }
+    void inThongTinDog(){
+        inThongTin();
+        cout << "-Mau Long: " << mauLong;
     }
     void Sua(){
         cout << "gau gau";
     }
 };
 
+class Poodle : public dog{
+    public: 
+    Poodle(){}
+    Poodle(int id, int soChan, string nameAnimail ,float canNang, float chieuCao, string mauLong){
+        this->id = id;
+        this->soChan = soChan;
+        this->nameAnimail = nameAnimail;
+        this->name = "Cho Co";
+        this->canNang = canNang;
+        this->chieuCao = chieuCao;
+        this->mauLong = mauLong;
+    }
+    void SanMoi(){
+        cout << "Sua gaugau";
+    }
+
+};
+
+class chihuahua : public dog{
+    public:
+    chihuahua(){}
+    chihuahua(int id, int soChan, string nameAnimail, string name, float canNang, float chieuCao, string mauLong){
+        this->id = id;
+        this->soChan = soChan;
+        this->nameAnimail = nameAnimail;
+        this->name = name;
+        this->canNang = canNang;
+        this->chieuCao = chieuCao;
+        this->mauLong = mauLong;
+    }
+    void ThoiGianTam(){
+        cout << "5 phut";
+    }
+    void TuVe(){
+        cout << "\nSua Gaugau";
+    }
+    void Ngu(){
+        cout << "\nKho Kho";
+    }
+    void Ban(){
+        cout << "\nGia Ban: 5000$";
+    }
+};
+
+
 int main(){
     // Animail DongVat = Animail(1,4,"Ga Cong Ngiep", "Gia Xuc", 4.5, 0.4);
     // DongVat.inThongTin();
-    dog ben(1,4,"Ga Cong Ngiep", "Gia Xuc", 4.5, 0.4);
+    // dog ben(1,4,"Ga Cong Ngiep", "Gia Xuc", 4.5, 0.4, "Den");
+    // ben.inThongTinDog();
+
+    chihuahua ben(1,4, "chihuahua" , "Cho Co", 4.5, 0.4, "Den");
+    // ben.inThongTinDog();
+    ben.TuVe();
+    ben.Ngu();
+    ben.Ban();
+
     return 0;
 }
